fec2f785ddb594a1062c73167c59d60a63c069e5
Michael O. <mcvavy@gmail.com>
2025-04-05 13:24:04 +0100

    Combined operators and crd deployment with usage in one workflow

--- Base/Operators/provider.tf
+++ Base/Operators/provider.tf
@@ -1,9 +1,25 @@
 terraform {
     required_version = ">= 1.0.0"
     required_providers {
+
+    azurerm = {
+        source  = "hashicorp/azurerm"
+        version = "~>4.0"
+    }
+
+    kubernetes = {
+        source  = "hashicorp/kubernetes"
+        version = "2.33.0"
+    }
+
     helm = {
         source  = "hashicorp/helm"
         version = "2.16.1"
+    }
+
+    external = {
+      source  = "hashicorp/external"
+      version = "2.3.4"
     }
   }
 
@@ -15,10 +31,47 @@ terraform {
   }
 }
 
+provider "azurerm" {
+  resource_provider_registrations = "all"
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+# Kubernetes provider using OIDC token
+provider "kubernetes" {
+  # config_path = "~/.kube/config" # Update with your kubeconfig path
+  host  = var.k8s_host
+  token = data.external.k8s_token.result.token
+}
+
+data "external" "k8s_token" {
+  program = ["sh", "-c", <<-EOT
+    token=$(curl -s ${var.keycloak_issuer_url}/protocol/openid-connect/token \
+      -d client_id=${var.keycloak_client_id} \
+      -d client_secret=${var.keycloak_client_secret} \
+      -d grant_type=client_credentials | jq -r .access_token)
+    echo "{\"token\":\"$token\"}"
+  EOT
+  ]
+}
+
+# Helm provider using OIDC token
 provider "helm" {
   kubernetes {
-    # config_path = "~/.kube/config" # Update with your kubeconfig path
     host  = var.k8s_host
-    token = var.k8s_token
+    token = data.external.k8s_token.result.token
   }
+}
+
+# kubectl provider using OIDC token
+provider "kubectl" {
+  load_config_file = "false"
+  host  = var.k8s_host
+  token = data.external.k8s_token.result.token
+}
+
+provider "external" {
 }
--- Base/Operators/variables.tf
+++ Base/Operators/variables.tf
@@ -1,7 +1,29 @@
+# Kubernetes variables
 variable "k8s_host" {
   type = string
+  description = "Kubernetes host"
+  default = "https://k8s-server.labgrid.net"
 }
 
 variable "k8s_token" {
   type = string
+  description = "Kubernetes token"
+}
+
+variable "keycloak_issuer_url" {
+  type        = string
+  description = "Keycloak issuer URL"
+  sensitive   = true
+}
+
+variable "keycloak_client_secret" {
+  type        = string
+  description = "Keycloak client secret"
+  sensitive   = true
+}
+
+variable "keycloak_client_id" {
+  type        = string
+  description = "Keycloak client id"
+  sensitive   = true
 }
