25a2e16e5ee6a6dd833d36716c2819269cd07293
Cloudsty <wi13b051@gmail.com>
2026-04-01 18:25:39 +0100

    fix: temporarily comment out external secrets

--- Base/main.tf
+++ Base/main.tf
@@ -48,30 +48,30 @@ resource "helm_release" "ingress_nginx" {
 }
 
 resource "kubernetes_secret_v1" "cloudflare_token_secret" {
-  depends_on = [ kubernetes_manifest.metallb_l2advertisement ]
+  depends_on = [kubernetes_manifest.metallb_l2advertisement]
 
   metadata {
-    name = "cloudflare-token-secret"
-    namespace = "${local.certManagerSettings.namespace}"
+    name      = "cloudflare-token-secret"
+    namespace = local.certManagerSettings.namespace
   }
 
   data = {
-    cloudflare-token: "${var.cloudflareApiTokenKey}"
+    cloudflare-token : "${var.cloudflareApiTokenKey}"
   }
 
   type = "Opaque"
 }
 
 resource "kubernetes_secret_v1" "tranzr_cloudflare_token_secret" {
-  depends_on = [ kubernetes_manifest.metallb_l2advertisement ]
+  depends_on = [kubernetes_manifest.metallb_l2advertisement]
 
   metadata {
-    name = "tranzr-cloudflare-token-secret"
-    namespace = "${local.certManagerSettings.namespace}"
+    name      = "tranzr-cloudflare-token-secret"
+    namespace = local.certManagerSettings.namespace
   }
 
   data = {
-    cloudflare-token: "${var.tranzrCloudflareApiTokenKey}"
+    cloudflare-token : "${var.tranzrCloudflareApiTokenKey}"
   }
 
   type = "Opaque"
@@ -88,8 +88,8 @@ resource "kubernetes_manifest" "letsencrypt-staging" {
     }
     spec = {
       acme = {
-        server  = local.clusterIssuerSettings.stagingServer
-        email   = local.clusterIssuerSettings.email
+        server = local.clusterIssuerSettings.stagingServer
+        email  = local.clusterIssuerSettings.email
         privateKeySecretRef = {
           name = local.clusterIssuerSettings.nameStaging
         }
@@ -123,8 +123,8 @@ resource "kubernetes_manifest" "tranzr-letsencrypt-staging" {
     }
     spec = {
       acme = {
-        server  = local.clusterIssuerSettings.stagingServer
-        email   = local.clusterIssuerSettings.email
+        server = local.clusterIssuerSettings.stagingServer
+        email  = local.clusterIssuerSettings.email
         privateKeySecretRef = {
           name = local.clusterIssuerSettings.tranzrNameStaging
         }
@@ -158,8 +158,8 @@ resource "kubernetes_manifest" "letsencrypt-production" {
     }
     spec = {
       acme = {
-        server  = local.clusterIssuerSettings.productionServer
-        email   = local.clusterIssuerSettings.email
+        server = local.clusterIssuerSettings.productionServer
+        email  = local.clusterIssuerSettings.email
         privateKeySecretRef = {
           name = local.clusterIssuerSettings.nameProduction
         }
@@ -193,8 +193,8 @@ resource "kubernetes_manifest" "tranzr-letsencrypt-production" {
     }
     spec = {
       acme = {
-        server  = local.clusterIssuerSettings.productionServer
-        email   = local.clusterIssuerSettings.email
+        server = local.clusterIssuerSettings.productionServer
+        email  = local.clusterIssuerSettings.email
         privateKeySecretRef = {
           name = local.clusterIssuerSettings.tranzrNameProduction
         }
@@ -253,7 +253,7 @@ resource "kubernetes_manifest" "synology-csi-namespace" {
 }
 
 resource "kubectl_manifest" "client-info-secret" {
-    yaml_body = <<YAML
+  yaml_body  = <<YAML
 apiVersion: v1
 kind: Secret
 metadata:
@@ -269,7 +269,7 @@ stringData:
       username: ${local.synologyCsiSettings.serviceAccountUsername}
       password: ${local.synologyCsiSettings.serviceAccountPassword}
 YAML
-depends_on = [kubernetes_manifest.synology-csi-namespace]
+  depends_on = [kubernetes_manifest.synology-csi-namespace]
 }
 
 # resource "kubernetes_secret_v1" "client-info-secret" {
@@ -292,17 +292,17 @@ depends_on = [kubernetes_manifest.synology-csi-namespace]
 
 
 resource "helm_release" "synology-csi-chart" {
-  name             = local.synologyCsiSettings.name
-  repository       = local.synologyCsiSettings.repository
-  chart            = local.synologyCsiSettings.name
-  version          = local.synologyCsiSettings.chart_version
-  namespace        = local.synologyCsiSettings.namespace
+  name       = local.synologyCsiSettings.name
+  repository = local.synologyCsiSettings.repository
+  chart      = local.synologyCsiSettings.name
+  version    = local.synologyCsiSettings.chart_version
+  namespace  = local.synologyCsiSettings.namespace
 
-  values = [ 
-    file("${path.module}/values/synology/values.yaml") 
-    ]
+  values = [
+    file("${path.module}/values/synology/values.yaml")
+  ]
 
-  wait = true
+  wait    = true
   timeout = 300
 
   depends_on = [kubectl_manifest.client-info-secret]
@@ -315,14 +315,14 @@ resource "kubernetes_storage_class_v1" "synology-iscsi-delete" {
       "storageclass.kubernetes.io/is-default-class" = "false"
     }
   }
-  storage_provisioner = "csi.san.synology.com"
-  reclaim_policy      = "Delete"
+  storage_provisioner    = "csi.san.synology.com"
+  reclaim_policy         = "Delete"
   allow_volume_expansion = true
   parameters = {
     dsm = var.synologyClientIp
-    fsType: "btrfs"
-    location = "/volume1"
-    protocol = "iscsi"
+    fsType : "btrfs"
+    location      = "/volume1"
+    protocol      = "iscsi"
     formatOptions = "--nodiscard"
   }
 }
@@ -334,15 +334,15 @@ resource "kubernetes_storage_class_v1" "synology-iscsi-delete-wffc" {
       "storageclass.kubernetes.io/is-default-class" = "true"
     }
   }
-  storage_provisioner = "csi.san.synology.com"
-  reclaim_policy      = "Delete"
+  storage_provisioner    = "csi.san.synology.com"
+  reclaim_policy         = "Delete"
   allow_volume_expansion = true
-  volume_binding_mode = "WaitForFirstConsumer"
+  volume_binding_mode    = "WaitForFirstConsumer"
   parameters = {
     dsm = var.synologyClientIp
-    fsType: "btrfs"
-    location = "/volume1"
-    protocol = "iscsi"
+    fsType : "btrfs"
+    location      = "/volume1"
+    protocol      = "iscsi"
     formatOptions = "--nodiscard"
   }
 }
@@ -354,20 +354,20 @@ resource "kubernetes_storage_class_v1" "synology-nfs-delete" {
       "storageclass.kubernetes.io/is-default-class" = "false"
     }
   }
-  storage_provisioner = "csi.san.synology.com"
-  reclaim_policy      = "Delete"
+  storage_provisioner    = "csi.san.synology.com"
+  reclaim_policy         = "Delete"
   allow_volume_expansion = true
   parameters = {
-    dsm = var.synologyClientIp
-    location = "/volume1"
-    protocol = "nfs"
+    dsm              = var.synologyClientIp
+    location         = "/volume1"
+    protocol         = "nfs"
     mountPermissions = "0775"
   }
   mount_options = ["nfsvers=4.1"]
 }
 
 resource "kubectl_manifest" "synology-snapshot-class" {
-    yaml_body = <<YAML
+  yaml_body  = <<YAML
 apiVersion: snapshot.storage.k8s.io/v1
 kind: VolumeSnapshotClass
 metadata:
@@ -377,11 +377,11 @@ metadata:
 driver: csi.san.synology.com
 deletionPolicy: Delete
 YAML
-depends_on = [  kubernetes_manifest.synology-csi-namespace ]
+  depends_on = [kubernetes_manifest.synology-csi-namespace]
 }
 
 resource "helm_release" "argocd" {
-  depends_on = [ kubernetes_manifest.metallb_l2advertisement ]
+  depends_on = [kubernetes_manifest.metallb_l2advertisement]
 
   name             = local.argocdSettings.name
   repository       = local.argocdSettings.repository
@@ -390,9 +390,9 @@ resource "helm_release" "argocd" {
   version          = local.argocdSettings.chart_version
   create_namespace = true
 
-  values = [ 
-    file("${path.module}/values/argocd/values.yaml") 
-    ]
+  values = [
+    file("${path.module}/values/argocd/values.yaml")
+  ]
 }
 
 resource "kubernetes_secret_v1" "azure-secret-sp-secret" {
@@ -401,116 +401,101 @@ resource "kubernetes_secret_v1" "azure-secret-sp-secret" {
   }
 
   data = {
-    clientId = "${var.azureServicePrincipalClientId}"
+    clientId     = "${var.azureServicePrincipalClientId}"
     clientSecret = "${var.azureServicePrincipalClientSecret}"
   }
 
   type = "Opaque"
 }
 
-resource "kubernetes_manifest" "azure-kv-cluster-store" {
-  depends_on = [kubernetes_secret_v1.azure-secret-sp-secret]
+# resource "kubernetes_manifest" "azure-kv-cluster-store" {
+#   depends_on = [kubernetes_secret_v1.azure-secret-sp-secret]
 
-  manifest = {
-    apiVersion = "external-secrets.io/v1"
-    kind       = "ClusterSecretStore"
-    metadata = {
-      name = "azure-kv-cluster-store"
-    }
-    spec = {
-      provider = {
-        azurekv = {
-          tenantId = var.azureServicePrincipalTenantId
-          vaultUrl = var.azureKeyVaultUrl
-          authSecretRef = {
-            # points to the secret that contains
-            # the azure service principal credentials
-            clientId = {
-              name = "azure-secret-sp-secret"
-              key = "clientId"
-              namespace = "default"
-            }
-            clientSecret = {
-              name = "azure-secret-sp-secret"
-              key = "clientSecret"
-              namespace = "default"
-            }
-          }
-        }
-      }
-    }
-  }
-}
+#   manifest = {
+#     apiVersion = "external-secrets.io/v1"
+#     kind       = "ClusterSecretStore"
+#     metadata = {
+#       name = "azure-kv-cluster-store"
+#     }
+#     spec = {
+#       provider = {
+#         azurekv = {
+#           tenantId = var.azureServicePrincipalTenantId
+#           vaultUrl = var.azureKeyVaultUrl
+#           authSecretRef = {
+#             # points to the secret that contains
+#             # the azure service principal credentials
+#             clientId = {
+#               name = "azure-secret-sp-secret"
+#               key = "clientId"
+#               namespace = "default"
+#             }
+#             clientSecret = {
+#               name = "azure-secret-sp-secret"
+#               key = "clientSecret"
+#               namespace = "default"
+#             }
+#           }
+#         }
+#       }
+#     }
+#   }
+# }
 
-resource "kubernetes_manifest" "pg-admin-namespace" {
-  manifest = {
-    apiVersion = "v1"
-    kind       = "Namespace"
-    metadata = {
-      name = local.pgAdminSettings.namespace
-    }
-  }
-}
+# resource "kubernetes_manifest" "pg-admin-namespace" {
+#   manifest = {
+#     apiVersion = "v1"
+#     kind       = "Namespace"
+#     metadata = {
+#       name = local.pgAdminSettings.namespace
+#     }
+#   }
+# }
 
-resource "kubernetes_manifest" "pg-admin-password" {
-  depends_on = [ kubernetes_manifest.pg-admin-namespace ]
+# resource "kubernetes_manifest" "pg-admin-password" {
+#   depends_on = [ kubernetes_manifest.pg-admin-namespace ]
 
-  manifest = {
-    apiVersion = "external-secrets.io/v1"
-    kind       = "ExternalSecret"
-    metadata = {
-      name = "pg-admin-password"
-      namespace = kubernetes_manifest.pg-admin-namespace.manifest.metadata.name
-    }
-    spec = {
-      secretStoreRef = {
-        name = "azure-kv-cluster-store"
-        kind = "ClusterSecretStore"
-      }
-      refreshInterval = "36h"
-      target = {
-        name     = "pg-admin-password-secret"
-        template = {
-          type = "kubernetes.io/basic-auth"
-        }
-      }
-      data = [
-        {
-          secretKey = "password"
-          remoteRef = {
-            key = "pg-admin-password"
-          }
-        }
-      ]
-    }
-  }
-}
+#   manifest = {
+#     apiVersion = "external-secrets.io/v1"
+#     kind       = "ExternalSecret"
+#     metadata = {
+#       name = "pg-admin-password"
+#       namespace = kubernetes_manifest.pg-admin-namespace.manifest.metadata.name
+#     }
+#     spec = {
+#       secretStoreRef = {
+#         name = "azure-kv-cluster-store"
+#         kind = "ClusterSecretStore"
+#       }
+#       refreshInterval = "36h"
+#       target = {
+#         name     = "pg-admin-password-secret"
+#         template = {
+#           type = "kubernetes.io/basic-auth"
+#         }
+#       }
+#       data = [
+#         {
+#           secretKey = "password"
+#           remoteRef = {
+#             key = "pg-admin-password"
+#           }
+#         }
+#       ]
+#     }
+#   }
+# }
 
-resource "helm_release" "pgadmin" {
-  depends_on = [ kubernetes_manifest.pg-admin-password ]
+# resource "helm_release" "pgadmin" {
+#   depends_on = [ kubernetes_manifest.pg-admin-password ]
 
-  name             = local.pgAdminSettings.name
-  repository       = local.pgAdminSettings.repository
-  chart            = local.pgAdminSettings.name
-  namespace        = kubernetes_manifest.pg-admin-namespace.manifest.metadata.name
-  version          = local.pgAdminSettings.chart_version
-
-  values = [ 
-    file("${path.module}/values/pgadmin/values.yaml") 
-    ]
-}
-
-# resource "helm_release" "prometheus" {
-#   depends_on = [ kubernetes_manifest.metallb_l2advertisement ]
-
-#   name             = local.prometheusSettings.name
-#   repository       = local.prometheusSettings.repository
-#   chart            = local.prometheusSettings.name
-#   namespace        = local.prometheusSettings.namespace
-#   version          = local.prometheusSettings.chart_version
-#   create_namespace = true
+#   name             = local.pgAdminSettings.name
+#   repository       = local.pgAdminSettings.repository
+#   chart            = local.pgAdminSettings.name
+#   namespace        = kubernetes_manifest.pg-admin-namespace.manifest.metadata.name
+#   version          = local.pgAdminSettings.chart_version
 
 #   values = [ 
-#     file("${path.module}/values/prometheus/values.yaml") 
+#     file("${path.module}/values/pgadmin/values.yaml") 
 #     ]
 # }
